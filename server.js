e88ae6f5f77a89cf1025ead2ba00103ed34b5c30
Ubuntu <ubuntu@ip-172-31-31-144.ec2.internal>
2016-12-31 03:08:48 +0000

    added server

---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require('express')
+var fs = require('fs')
+var path = require('path')
+var execSync = require('child_process').execSync;
+var app = express()
+var exec = require('child_process').exec;
+
+var wordb =  JSON.parse(fs.readFileSync("static/wordcnt.json")) 
+
+clipsdir = "data/Clips/"
+
+concatT = "concat:"
+
+var staticPath = path.join(__dirname, '/static');
+app.use(express.static(staticPath));
+
+app.get('/request', function (req, res) {
+
+  console.log("Request Received")
+  
+  var numAppear;
+  var filen;
+  var startTime;
+  var stopTime;
+  var iter = 0;
+  var filesClipped = 0;  
+  
+  res.send('Processing')
+  var Phrase = req.query.phrase.split(" ")
+  var ID = req.query.id
+  var numWord = Phrase.length
+  var converts = 0
+  console.log(req.query.phrase)
+  Phrase.forEach(function(element){ 
+
+    if(typeof wordb[element] == 'undefined'){
+    
+      console.log("word unavailible")
+      numWord = numWord - 1;
+
+    }
+    else{
+ 
+      numAppear = wordb[element].loc.length -1
+      numAppear = Math.round(Math.random() * (numAppear));
+      name = wordb[element].loc[numAppear]
+      ts = wordb[element].start[numAppear] 
+      te = wordb[element].end[numAppear] 
+      //execSync('ffmpeg -i '+ "data/SourceVids/Trump/"+ name +'.mp4 -ss '+ ts +' -to '+ te +' -vcodec mjpeg '+clipsdir+iter+'.avi -y')
+      concatT = concatT +clipsdir+iter+".avi|"        
+      exec('ffmpeg -i ' + "data/SourceVids/Trump/" + name + '.mp4 -ss ' + ts + ' -to ' + te +' -vcodec mjpeg '+clipsdir+iter+'.avi -y',function(err, out, code){
+        numWord = numWord - 1
+        console.log(numWord)  
+        if(0 == numWord){
+          exec("ffmpeg -i \"" + concatT.slice(0,-1) + "\" -c copy " + ID + ".avi -y",function(err2,out2,code2){
+          })
+        }
+      })
+      iter = iter + 1
+    }
+
+
+  });
+
+})
+
+app.listen(80, function () {
+  console.log('Listening')
+})
